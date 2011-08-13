bf34f82424809b3e09d0f56585e8d50dbc190aba
Wen-Tien Chang <ihower@gmail.com>
2011-08-13 11:05:18 +0800

    Init Rails 3.1.0.rc5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkplusRails::Application.config.secret_token = 'dca3bbfba08868d4a0a023eb8076163aca35e1825b120fa6413e0919eb7d10b59ed6d9a7978a0a174de4f78c6915c68fd908e8a408b07ffd5d67da4164ec0a00'
